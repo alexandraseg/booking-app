308689d286f9df11c43bd9712cc7dbbfa32a7a5e
Lexie <alex.segkou@gmail.com>
2023-06-04 19:59:58 +0300

    fixed admin button and what can be seen in AccountNav

--- client/src/AccountNav.jsx
+++ client/src/AccountNav.jsx
@@ -1,7 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 export default function AccountNav(){
 
+const { user } = useContext(UserContext);
 // const location = useLocation();
 // console.log(location); ----> this shows the following properties: hash, key, pathname, search, state
 const {pathname} = useLocation(); //grabbing pathname
@@ -23,6 +26,8 @@ function linkClasses (type=null) {
             }
     return classes;
 }
+
+const isAdmin = user?.email === 'admin@gmail.com';
     
   return(
     <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
@@ -32,7 +37,10 @@ function linkClasses (type=null) {
             </svg>
             My profile
         </Link>
-        <Link className={linkClasses('bookings')} to={'/account/bookings'}>
+
+        {!isAdmin && (
+            <>
+            <Link className={linkClasses('bookings')} to={'/account/bookings'}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
             </svg>
@@ -44,6 +52,24 @@ function linkClasses (type=null) {
             </svg>
             My accommodations
         </Link>
+            </>
+        )
+
+        }
+
+        {isAdmin && (
+            <>
+            <Link className={linkClasses('dashboard')} to={'/dashboard'}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+            </svg>
+            My dashboard
+        </Link>
+            </>
+        )
+
+        }
+        
     </nav>
   );  
 }
--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area, ResponsiveContainer} from "recharts";
 import NavigationDashboard from "./NavigationDashboard";
+import { Link } from "react-router-dom";
 
 export default function DashboardPage() {
 
@@ -58,7 +59,7 @@ export default function DashboardPage() {
                         <div className="w-1/2 grow">
                             <h1 className="text-secondary text-xl ">Welcome, <b>Admin</b></h1>
                         </div>
-                        <div className="">
+                        <Link to={'/account'}>
                             <div className="bg-gray-200 flex items-center rounded-md mr-7">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
@@ -66,7 +67,7 @@ export default function DashboardPage() {
                                 <span className="px-3">Admin</span>
                             </div>
                             
-                        </div>
+                        </Link>
                     </div>
 
                     <div className="flex gap-5">
